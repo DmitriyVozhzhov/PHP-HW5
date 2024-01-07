1e3d2a781c537e5c207b2efc993f539fce77ecbf
DmitriyVozhzhov <Dmitriy.Vozhzhov@gmail.com>
2024-01-07 16:21:49 +0200

    fix HW index.php

--- index.php
+++ index.php
@@ -5,25 +5,17 @@ declare(strict_types=1);
 //1. площа кола
 //function areaOfACircle(int $r)
 //{
-//    $p = 3.14;
+//    $p = pi();
 //    $S = $p * ($r ** 2);
 //    echo "$S" . PHP_EOL;
 //}
 //areaOfACircle(5);
 
-// площа кола, варіант 2
-//function areaOfACircle2(int $r, $p=3.14 )
-//{
-//    $S = $p * ($r ** 2);
-//    echo "$S" . PHP_EOL;
-//}
-//areaOfACircle2(5);
-
-// площа кола, варіант 3 => стрілочка ф-ція
-//$areaOfACircle3 = fn ($r, $p=3.14) =>  $S = $p * ($r ** 2);
-// echo $areaOfACircle3(5) . PHP_EOL;
-
-
+// площа кола, варіант 2 => стрілочка ф-ція
+//$p = pi();
+//$areaOfACircle3 = fn ($r, $p) =>  $p * ($r ** 2);
+// echo $areaOfACircle3(5, $p) . PHP_EOL;
+//
 
 
 // 2.піднесення до степеня
@@ -44,21 +36,20 @@ declare(strict_types=1);
 //3.функція змінює передане число
 
 //$number = 12;
-//function division( int|float &$number, int $div):int|float
+//function division( int|float &$number, int $div):void
 //{
-//    return $number = $number / $div;
+//     $number = $number / $div;
 //}
 //
-//echo  $number . PHP_EOL;
 //division($number, 2);
 //echo  $number . PHP_EOL;
 
 
 //3.функція повертає нове число
-$number = 10;
-function division2( int|float $number, int $div ):int|float
-{
-    return $newNumber = $number / $div;
-}
-
-echo division2($number, 5) . PHP_EOL;
+//$number = 10;
+//function division2( int|float $number, int $div ):int|float
+//{
+//    return $newNumber = $number / $div;
+//}
+//
+//echo division2($number, 5) . PHP_EOL;
